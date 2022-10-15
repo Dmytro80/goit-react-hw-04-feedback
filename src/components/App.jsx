f4ddeab60ce010f051c497f158978d760678c136
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-10-15 13:35:53 +0200

    add markup step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <div>
+          {options.map(option => {
+            return (
+              <button
+                type="button"
+                key={option}
+                onClick={() => this.handleIncrement(option)}
+              >
+                {option}
+              </button>
+            );
+          })}
+        </div>
+
+        <h2>Statistics</h2>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+      </div>
+    );
+  }
+}
