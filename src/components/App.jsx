f2a2370f2d2a93fe14b288c9539e3957647696c0
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-10-28 12:02:17 +0200

    add useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Statistics from './statistics';
 import FeedbackOptions from './feedbackOptions';
 import Section from './section';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const options = Object.keys(state);
+
+  const { good, neutral, bad } = state;
+
+  const handleIncrement = option => {
+    setState(prevState => ({ ...prevState, [option]: prevState[option] + 1 }));
   };
 
-  handleIncrement = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const values = Object.values(this.state);
+  const countTotalFeedback = () => {
+    const values = Object.values(state);
     return values.reduce((total, value) => {
       return total + value;
     }, 0);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
+  const countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = state;
 
     return Math.round((100 / (good + neutral + bad)) * good);
   };
 
-  render() {
-    const options = Object.keys(this.state);
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={handleIncrement} />
+      </Section>
 
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.handleIncrement}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback}
-            positivePercentage={this.countPositiveFeedbackPercentage}
-          />
-        </Section>
-      </>
-    );
-  }
-}
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback}
+          positivePercentage={countPositiveFeedbackPercentage}
+        />
+      </Section>
+    </>
+  );
+};
